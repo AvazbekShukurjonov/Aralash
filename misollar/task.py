6641419bd8a34e725251f5ec552b000a6e5e664a
Avazbek Shukurjonov <avazbek.shukurjonov@bk.ru>
2021-11-20 12:27:13 +0500

    Homeworks

---
+++ misollar/task.py
@@ -0,0 +1,53 @@
+# print(""""Nexia","Tico",'Dmas',ko\'rganlar qilar xavas""" )
+
+# print(5**4)
+
+# print(22%4)
+
+# kv_t=125
+# Per=print(125*4)
+# yuzi=print(125**4)
+
+# print(3.14*(12/2)**2)
+
+# katet1=6
+# katet2=7
+# sum=0
+# sum=(katet1**2)+(katet2**2)
+# print(sum)
+
+# a='hello world'
+# print(a)
+
+# xabar='Avaz'
+# print(xabar)
+# xabar='Avazbek'
+# print(xabar)
+
+
+# radius=5
+# pi=3,14
+# yuza= pi * radius**2
+# print("Radiusi",radius,"ga teng doira yuzi=",yuza)
+
+# kocha="Bog'bon"
+# mahalla="Sag'bon"
+# tuman="Bodomzor"
+# vilyat="Samarqand"
+# print(kocha + ' ' + 'ko\'chasi',mahalla + ' ' + 'mahallasi',tuman + ' '+'tumani',vilyat+' ''viloyati')
+
+
+# kocha=input('Ko\'changizni nomi\n')
+# mahalla=input('mahallangizni nomi\n')
+# tuman=input('tumaningizni nomi\n')
+# viloyat=input('viloyatingiz nomi\n')
+# print(kocha + ' ' + 'ko\'chasi',mahalla + ' ' + 'mahallasi',tuman + ' '+'tumani', viloyat +' ''viloyati')
+
+
+# kocha=input('Ko\'changizni nomi\n')
+# mahalla=input('mahallangizni nomi\n')
+# tuman=input('tumaningizni nomi\n')
+# viloyat=input('viloyatingiz nomi\n')
+# print(kocha + 'ko\'chasi\n',mahalla + 'mahallasi\n',tuman + 'tumani\n', viloyat +'viloyati\n')
+
+
